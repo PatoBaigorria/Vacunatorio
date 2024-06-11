5ab9ea70127294259460d713c075def39aaf0e82
EzequielDiaz777 <diazezequiel777@gmail.com>
2024-06-11 01:10:02 -0300

    Se implemento la autenticación en las rutas

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,8 +1,5 @@
 const express = require("express");
-//const bcrypt = require("bcrypt");
 const router = express.Router();
-//const passport = require("passport");
-//const Usuario = require("../models/usuario");
 const aplicacionRoutes = require("./aplicacionRoutes");
 const centrodevacunacionRoutes = require("./centrodevacunacionRoutes");
 const depositonacionalRoutes = require("./depositonacionalRoutes");
@@ -14,90 +11,66 @@ const loteproveedorRoutes = require("./loteproveedorRoutes");
 const personaRoutes = require("./personaRoutes");
 const trasladoRoutes = require("./trasladoRoutes");
 const usuarioRoutes = require("./usuarioRoutes");
-//const { passport } = require('../config/passportConfig');
 
 const { passport } = require("../app");
+
+// Middleware para verificar si el usuario está autenticado
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+// Middleware para añadir el estado de autenticación a todas las vistas
+function addAuthenticationStatus(req, res, next) {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+}
+
+// Usar el middleware globalmente
+router.use(addAuthenticationStatus);
+
 // Ruta de la página de inicio (pública)
 router.get("/", (req, res) => {
-	res.render("index");
+  res.render("index");
 });
 
-// login route
-router.post("/login", passport.authenticate("local", {
-	successRedirect: "/usuarios/profile",
-	successMessage: "logueado",
-	successFlash: true,
-	failureRedirect: "/",
-	failureFlash: true,
-}));
+// Login route
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/usuarios/profile",
+    successMessage: "logueado",
+    successFlash: true,
+    failureRedirect: "/",
+    failureFlash: true,
+  })
+);
 
 router.get("/usuarios/viewUsuario", function (req, res, next) {
-	passport.authenticate("local", function (err, user, info, status) {
-		if (err) {
-			return next(err);
-		}
-		if (!user) {
-			return res.redirect("/signin");
-		}
-		res.redirect("/usuarios/viewUsuario");
-	})(req, res, next);
+  passport.authenticate("local", function (err, user, info, status) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/signin");
+    }
+    res.redirect("/usuarios/viewUsuario");
+  })(req, res, next);
 });
-
-router.use("/aplicaciones", aplicacionRoutes);
-router.use("/centrosdevacunacion", centrodevacunacionRoutes);
-router.use("/depositosnacionales", depositonacionalRoutes);
-router.use("/depositosprovinciales", depositoprovincialRoutes);
-router.use("/descartes", descarteRoutes);
-router.use("/laboratorios", laboratorioRoutes);
-router.use("/lotesinternos", loteinternoRoutes);
-router.use("/lotesproveedores", loteproveedorRoutes);
-router.use("/personas", personaRoutes);
-router.use("/traslados", trasladoRoutes);
-router.use("/usuarios", usuarioRoutes);
 
 // Rutas protegidas (requieren autenticación)
-/*router.get("/aplicaciones", isAuthenticated, (req, res) => {
-	router.use("/aplicaciones", aplicacionRoutes);
-});
-
-router.get("/centrosdevacunacion", isAuthenticated, (req, res) => {
-	router.use("/centrosdevacunacion", centrodevacunacionRoutes);
-});
-
-router.get("/depositosnacionales", isAuthenticated, (req, res) => {
-	router.use("/depositosnacionales", depositonacionalRoutes);
-})
-
-router.get("/depositosprovinciales", isAuthenticated, (req, res) => {
-	router.use("/depositosprovinciales", depositoprovincialRoutes);
-})
-
-router.get("/descartes", isAuthenticated, (req, res) => {
-	router.use("/descartes", descarteRoutes);
-})
-
-router.get("/laboratorios", isAuthenticated, (req, res) => {
-	router.use("/laboratorios", laboratorioRoutes);
-})
-
-router.get("/lotesinternos", isAuthenticated, (req, res) => {
-	router.use("/lotesinternos", loteinternoRoutes);
-})
-
-router.get("/lotesproveedor", isAuthenticated, (req, res) => {
-	router.use("/lotesproveedor", loteproveedorRoutes);
-})
-
-router.get("/personas", isAuthenticated, (req, res) => {
-	router.use("/personas", personaRoutes);
-})
-
-router.get("/traslados", isAuthenticated, (req, res) => {
-	router.use("/traslados", trasladoRoutes);
-})
-
-router.get("/usuarios", isAuthenticated, (req, res) => {
-	router.use("/usuarios", usuarioRoutes);
-})*/
+router.use("/aplicaciones", isAuthenticated, aplicacionRoutes);
+router.use("/centrosdevacunacion", isAuthenticated, centrodevacunacionRoutes);
+router.use("/depositosnacionales", isAuthenticated, depositonacionalRoutes);
+router.use("/depositosprovinciales", isAuthenticated, depositoprovincialRoutes);
+router.use("/descartes", isAuthenticated, descarteRoutes);
+router.use("/laboratorios", isAuthenticated, laboratorioRoutes);
+router.use("/lotesinternos", isAuthenticated, loteinternoRoutes);
+router.use("/lotesproveedores", isAuthenticated, loteproveedorRoutes);
+router.use("/personas", isAuthenticated, personaRoutes);
+router.use("/traslados", isAuthenticated, trasladoRoutes);
+router.use("/usuarios", isAuthenticated, usuarioRoutes);
 
 module.exports = router;
