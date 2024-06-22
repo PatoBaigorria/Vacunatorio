7dfc2a19977f35693bfdee0e04a38c14eeb61596
PatoBaigorria <87587522+PatoBaigorria@users.noreply.github.com>
2024-06-21 21:18:21 -0300

    Verificar toda la lógica de las vistas de traslado terminado

--- controllers/trasladoController.js
+++ controllers/trasladoController.js
@@ -30,6 +30,34 @@ const listarTraslados = async (req, res) => {
 };
 
 // Muestra formulario de creacion de Traslado
+const altaLogicaTraslado = async (req, res) => {
+	try {
+		await Traslado.update(
+			{
+				activo: 1,
+			},
+			{
+				where: {
+					idTraslado: req.params.id,
+				},
+			}
+		);
+		await createRegistro(
+			req.user.idUsuario,
+			"Traslado",
+			req.params.id,
+			"Alta"
+		);
+		req.flash("success", "Traslado dado de alta exitosamente.");
+		res.json({ success: true });
+	} catch (error) {
+		console.error("Error al dar de alta el traslado:", error);
+		req.flash("error", "Error al dar de alta el traslado.");
+		res.status(500).json({ success: false, message: error.message });
+	}
+};
+
+// Muestra formulario de creacion de Traslado
 const altaTraslado = async (req, res) => {
 	try {
 		const lotesInternos = await LoteInterno.findAll();
@@ -45,71 +73,89 @@ const altaTraslado = async (req, res) => {
 	}
 };
 
+
 const createTraslados = async (req, res) => {
-	try {
-		const { numeroDeSerie, idCentroDeVacunacion, fechaDeSalida } = req.body;
-		let fechaDeLlegada = req.body.fechaDeLlegada;
-		if (req.body.fechaDeLlegada == "") {
-			fechaDeLlegada = null;
-		} else {
-			fechaDeLlegada = req.body.fechaDeLlegada;
-		}
-		const traslado = await Traslado.create({
-			numeroDeSerie,
-			idCentroDeVacunacion,
-			fechaDeSalida,
-			fechaDeLlegada,
-			activo: 1,
-		});
-		if (fechaDeLlegada != null) {
-			const loteEncontrado = await LoteInterno.findOne({
-				where: { numeroDeSerie: numeroDeSerie },
-			});
-			console.log(loteEncontrado);
-			if (loteEncontrado) {
-				await loteEncontrado.update({
-					idCentroDeVacunacion: idCentroDeVacunacion,
-				});
-			}
-		}
-		await createRegistro(
-			req.user.idUsuario,
-			"Traslado",
-			traslado.dataValues.idTraslado,
-			"Creacion"
-		);
-		await createRegistro(
-			req.user.idUsuario,
-			"Traslado",
-			traslado.dataValues.idTraslado,
-			"Alta"
-		);
-		req.flash("success", "Traslado creado exitosamente");
-		res.redirect("/traslados");
-	} catch (error) {
-		res.status(500).json({
-			message: "Error al crear el traslado. " + error.message,
-		});
-	}
+    try {
+        const { numeroDeSerie, idCentroDeVacunacion, fechaDeSalida } = req.body;
+
+        // Verificación adicional
+        if (!numeroDeSerie || !idCentroDeVacunacion || !fechaDeSalida) {
+            req.flash('error', 'Todos los campos son obligatorios.');
+            return res.redirect('/traslados/crear');
+        }
+
+        let fechaDeLlegada = req.body.fechaDeLlegada;
+        if (req.body.fechaDeLlegada == "") {
+            fechaDeLlegada = null;
+        } else {
+            fechaDeLlegada = req.body.fechaDeLlegada;
+        }
+
+        const traslado = await Traslado.create({
+            numeroDeSerie,
+            idCentroDeVacunacion,
+            fechaDeSalida,
+            fechaDeLlegada,
+            activo: 1,
+        });
+
+        if (fechaDeLlegada != null) {
+            const loteEncontrado = await LoteInterno.findOne({
+                where: { numeroDeSerie: numeroDeSerie },
+            });
+            if (loteEncontrado) {
+                await loteEncontrado.update({
+                    idCentroDeVacunacion: idCentroDeVacunacion,
+                });
+            }
+        }
+
+        await createRegistro(
+            req.user.idUsuario,
+            "Traslado",
+            traslado.dataValues.idTraslado,
+            "Creacion"
+        );
+        await createRegistro(
+            req.user.idUsuario,
+            "Traslado",
+            traslado.dataValues.idTraslado,
+            "Alta"
+        );
+        req.flash("success", "Traslado creado exitosamente");
+        res.redirect("/traslados");
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al crear el traslado: " + error.message,
+        });
+    }
 };
 
 // Editar Traslado por ID
 const editTraslado = async (req, res) => {
-	try {
-		const traslado = await Traslado.findByPk(req.params.id);
-		const lotesInternos = await LoteInterno.findAll();
-		const centrosDeVacunacion = await CentroDeVacunacion.findAll();
-		res.render("traslado/editTraslado", {
-			traslado: traslado,
-			lotesInternos: lotesInternos,
-			centrosDeVacunacion: centrosDeVacunacion,
-		});
-	} catch (error) {
-		res.status(500).json({
-			message: "Error al obtener el Traslado." + error.message,
-		});
-	}
+    try {
+        const traslado = await Traslado.findByPk(req.params.id);
+        const lotesInternos = await LoteInterno.findAll();
+        const centrosDeVacunacion = await CentroDeVacunacion.findAll();
+
+        if (!traslado) {
+            req.flash('error', 'Traslado no encontrado.');
+            return res.redirect('/traslados');
+        }
+
+        res.render("traslado/editTraslado", {
+            traslado: traslado,
+            lotesInternos: lotesInternos,
+            centrosDeVacunacion: centrosDeVacunacion,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al obtener el Traslado: " + error.message,
+        });
+    }
 };
+
+
 
 // Actualizar un traslado por su ID
 const updateTraslado = async (req, res) => {
@@ -151,7 +197,7 @@ const deleteTrasladoFisica = async (req, res) => {
 	}
 };
 
-const deleteTrasladoLogica = async (req, res) => {
+const bajaTraslado = async (req, res) => {
 	try {
 		await Traslado.update(
 			{
@@ -159,7 +205,7 @@ const deleteTrasladoLogica = async (req, res) => {
 			},
 			{
 				where: {
-					DNI: req.params.id,
+					idTraslado: req.params.id,
 				},
 			}
 		);
@@ -170,7 +216,7 @@ const deleteTrasladoLogica = async (req, res) => {
 			"Baja"
 		);
 		req.flash("success", "Traslado dado de baja exitosamente.");
-		res.redirect("/traslados");
+		res.json({ success: true });
 	} catch (error) {
 		res.status(500).json({ message: "Error al dar de baja el traslado." });
 	}
@@ -178,10 +224,11 @@ const deleteTrasladoLogica = async (req, res) => {
 
 module.exports = {
 	listarTraslados,
+	altaLogicaTraslado,
 	altaTraslado,
 	createTraslados,
 	editTraslado,
 	updateTraslado,
 	deleteTrasladoFisica,
-	deleteTrasladoLogica,
+	bajaTraslado,
 };
--- routes/trasladoRoutes.js
+++ routes/trasladoRoutes.js
@@ -3,40 +3,14 @@ const router = express.Router();
 const trasladoController = require("../controllers/trasladoController");
 const authorize = require("../middleware/authorize");
 
-router.get(
-	"/",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.listarTraslados
-);
-router.get(
-	"/crear",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.altaTraslado
-);
-router.post(
-	"/",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.createTraslados
-);
-router.get(
-	"/:id",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.editTraslado
-);
-router.put(
-	"/:id",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.updateTraslado
-);
-router.delete(
-	"/:id",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.deleteTrasladoFisica
-);
-router.delete(
-	"/logica/:id",
-	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),
-	trasladoController.deleteTrasladoLogica
-);
+router.get("/",	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),	trasladoController.listarTraslados);
+router.get("/crear", authorize(["Super Admin", "Operador de logistica", "Agente de salud"]), trasladoController.altaTraslado);
+router.put("/:id/alta",	authorize(["Super Admin", "Operador de logistica", "Agente de salud"]),	trasladoController.altaLogicaTraslado);
+router.put("/:id/baja", authorize(["Super Admin", "Operador de logistica", "Agente de salud"]), trasladoController.bajaTraslado);
+router.post("/", authorize(["Super Admin", "Operador de logistica", "Agente de salud"]), trasladoController.createTraslados);
+router.get("/:id", authorize(["Super Admin", "Operador de logistica", "Agente de salud"]), trasladoController.editTraslado);
+router.put("/:id", authorize(["Super Admin", "Operador de logistica", "Agente de salud"]), trasladoController.updateTraslado);
+router.delete("/:id", authorize(["Super Admin", "Operador de logistica", "Agente de salud"]), trasladoController.deleteTrasladoFisica);
+
 
 module.exports = router;
